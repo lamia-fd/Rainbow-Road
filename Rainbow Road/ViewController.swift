81f382106c5d0cf8a6e7597b7406148f09586f7b
lamia <lamia.fd123@gmail.com>
2021-12-12 14:09:42 +0300

    v1

--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var color=[UIColor.red,UIColor.orange,UIColor.yellow,UIColor.green,UIColor.blue,UIColor.purple]
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource=self
+        tableView.rowHeight = 120 
         // Do any additional setup after loading the view.
     }
 
 
 }
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let call = tableView.dequeueReusableCell(withIdentifier: "call", for: indexPath)
+        
+        call.backgroundColor = color[indexPath.row]
+        
+        return call
+    }
+    
 
+    
+}
